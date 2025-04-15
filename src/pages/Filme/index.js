f7b0e8e1fac54acdc687fcd5fb4e6eb772b68699
Victor-Watanabe <watavh@hotmail.com>
2025-04-15 20:39:28 -0300

    feact: Bookmark and Alert System Design

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from '../../services/api';
 import "./filme.css";
+import { toast } from 'react-toastify'
 
 function Filme(){
     const { id } = useParams();
@@ -35,6 +36,24 @@ function Filme(){
         }
     },[navigate, id])
 
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix");
+
+        let filmesSalvos= JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some((filmesSalvo)=> filmesSalvo.id === filme.id)
+    
+        if (hasFilme){
+            toast.warn("Esse filme já está na sua lista!")
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        toast.success("Filme salvo com Sucesso!")
+
+    }
+
     if(loading){
         return(
             <div className="filme-info">
@@ -52,9 +71,9 @@ function Filme(){
             <strong>Avalição: {filme.vote_average} /10</strong>
         
             <div className="area-buttons">
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
-                    <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>Trailer</a>
+                    <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>Trailer</a>
                 </button>
             </div>
         </div>
